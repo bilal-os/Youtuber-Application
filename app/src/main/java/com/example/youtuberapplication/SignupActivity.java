5e1f36d86ec7f6e40191ec8fb161ba2a9c7696e5
zakiqasim100 <zakiqasim100@gmail.com>
2025-05-11 22:43:04 +0500

    Styled EditTexts with orange border, finalized Signup UI layout with navbar

--- app/src/main/java/com/example/youtuberapplication/SignupActivity.java
+++ app/src/main/java/com/example/youtuberapplication/SignupActivity.java
@@ -3,10 +3,17 @@ package com.example.youtuberapplication;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Html;
+import android.widget.TextView;
+
 public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        TextView termsText = findViewById(R.id.termsText);
+        String styledText = "By tapping Sign up you accept all <font color='#FFB10B'>terms</font> and <font color='#FFB10B'>conditions</font>";
+        termsText.setText(Html.fromHtml(styledText, Html.FROM_HTML_MODE_LEGACY));
+
     }
 }
